b5f2b00c2ddf9b1120a5ad86071f96bce28c430d
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-07-13 20:59:37 +0300

    added tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw'
 
-test('renders learn react link', () => {
+const server = setupServer(
+  rest.get('http://localhost:3001/banks', (req, res, ctx) => {
+      return res(ctx.json({
+        banks:
+          [
+            {
+              id: 1,
+              title: 'Tinkoff',
+              address: 'Moscow',
+              IBAN: 'CH7689144497354555744',
+              email: 'info@tinkoff.ru',
+              'Found Date': '12.04.2004'
+            },
+          ]
+      }))
+    }
+  )
+)
+
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test('render spinning', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole('status')).toBeInTheDocument();
+});
+
+test('renders fetched data', async () => {
+  render(<App />);
+  const IBANElement = screen.getByText('CH7689144497354555744');
+  expect(IBANElement).toBeInTheDocument();
 });
